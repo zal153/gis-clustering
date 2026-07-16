3d5864005b4605cf5012b85aa2d82fee3340e066
Rizal <rizalvanderoz12154@gmail.com>
2026-07-16 11:32:40 +0700

    Seed from SQL dump

--- database/seeders/NilaiWargaSeeder.php
+++ database/seeders/NilaiWargaSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use RuntimeException;
 
 class NilaiWargaSeeder extends Seeder
 {
@@ -12,6 +13,30 @@ class NilaiWargaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->seedTableFromDump('nilai_wargas');
+    }
+
+    private function seedTableFromDump(string $table): void
+    {
+        $dumpPath = base_path('gis_clustering.sql');
+        $sql = file_get_contents($dumpPath);
+
+        if ($sql === false) {
+            throw new RuntimeException("Unable to read SQL dump: {$dumpPath}");
+        }
+
+        $pattern = '/INSERT INTO `' . preg_quote($table, '/') . '`.*?;\R\R/s';
+
+        if (! preg_match($pattern, $sql, $matches)) {
+            throw new RuntimeException("Missing INSERT statement for table: {$table}");
+        }
+
+        $statement = $matches[0];
+
+        if (DB::getDriverName() === 'sqlite') {
+            $statement = str_replace("\\'", "''", $statement);
+        }
+
+        DB::unprepared($statement);
     }
 }
